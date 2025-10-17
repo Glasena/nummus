1ac366f4241bbb7e724b9d81e3758af46a686553
Glasena <alisonglasenapp@gmail.com>
2025-10-16 21:55:17 -0300

    Criado componente de grid de lançamentos.

---
+++ frontend/src/components/GridTransactions.vue
@@ -0,0 +1,31 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+const state = reactive({
+    transactions: []
+});
+
+const headers = [
+    { title: 'ID', align: 'start', key: 'id' },
+    { title: 'Descrição', align: 'start', key: 'description' },
+    { title: 'Valor', align: 'start', key: 'value' },
+    { title: 'Data', align: 'start', key: 'date' }
+]
+
+onMounted(async () => {
+    try {
+        const response = await axios.get("http://localhost:5010/transactions");
+        state.transactions = response.data
+        console.log("Fetched transactions:", state.transactions);
+    } catch (error) {
+        console.error("Error fetching transactions:", error);
+    }
+});
+
+</script>
+<template>
+    <v-data-table-virtual :headers="headers" :items="state.transactions" height="400" item-value="transaction"
+        fixed-header></v-data-table-virtual>
+</template>
