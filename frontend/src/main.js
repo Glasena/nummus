bb66e44391d549691d1730d2581f699102da57e4
Glasena <alisonglasenapp@gmail.com>
2025-10-16 21:53:14 -0300

    Adicionado configurações da biblioteca de tradução, de fonte e de ícones.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,13 +9,27 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'unfonts.css'
 
+//I18n - Traduções
+import { i18n } from './i18n.js'
+
+// Fonte padrão (Roboto)
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
+// Icones Material Design
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
 const app = createApp(App)
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
 })
 
 app.use(router);
 app.use(vuetify);
+app.use(i18n);
 app.mount('#app');
