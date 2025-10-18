6fab2ed6c68047ef27acfe454f1c3ae73b487f88
Glasena <alisonglasenapp@gmail.com>
2025-10-18 11:10:21 -0300

    Adiconado Pinia para state management e adicionado componente de input de data do vuetify.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'unfonts.css'
+import { VDateInput } from 'vuetify/labs/VDateInput'
 
 //I18n - Traduções
 import { i18n } from './i18n.js'
@@ -19,17 +20,26 @@ import '@fontsource/roboto/700.css'
 // Icones Material Design
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
+// Pinia - State Management
+import { createPinia } from 'pinia'
+
 const app = createApp(App)
 
 const vuetify = createVuetify({
-  components,
+  components: {
+    ...components, // mantém todos os componentes padrão
+    VDateInput,    // adiciona o manualmente
+  },
   directives,
   icons: {
     defaultSet: 'mdi',
   },
 })
 
+const pinia = createPinia();
+
 app.use(router);
 app.use(vuetify);
 app.use(i18n);
+app.use(pinia);
 app.mount('#app');
